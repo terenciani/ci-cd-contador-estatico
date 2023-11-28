fe6451642b9314fa3901bc4bd566b6356680a89b
terenciani <terenciani@outlook.com>
2023-11-28 20:36:28 -0300

    Update contador.test.js

--- __test__/contador.test.js
+++ __test__/contador.test.js
@@ -3,5 +3,5 @@ const ContadorServico = require("../src/servico/ContadorServico");
 
 test('Obter Contador!!', () => {
     let resp = ContadorServico.obterContador();
-    expect(resp.contador).toBe(0)
+    expect(resp.contador).toBe(1)
 })
